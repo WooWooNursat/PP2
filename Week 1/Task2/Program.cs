4a5e0227f4f75dc03da93b7c27b0aacbd749bffe
tspenguin <tspenguin500@gmail.com>
2019-02-02 12:49:37 +0600

    'init'

---
+++ Week 1/Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string line = Console.ReadLine();
+            string[] arr = line.Split();
+            for(int j=0;j<arr.Length;j++)
+            {
+                for(int i=0;i<2;i++)
+                {
+                    Console.Write(arr[j] + " ");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Week 1/Task4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task4
+{
+    class Student
+    {
+        public string name;
+        public int id;
+        public int year = 0;
+        
+        public Student(string n, int i)
+        {
+            name = n;
+            id = i;
+        }
+        public void PrintInfo()
+        {
+            for(int i = 0; i < 4; i++)
+            {
+                Console.WriteLine(name + " " + id + " " + ++year);
+            }
+            
+        }
+       
+
+
+
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student("Nurik", 7);
+            s.PrintInfo();
+            Console.ReadKey();
+        }
+    }
+}
